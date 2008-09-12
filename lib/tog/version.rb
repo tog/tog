79de908728b48774a5b6a106ae3b71dc0e8651c7
Aitor García <aitor.garcia@gmail.com>
2008-09-12 12:30:48 +0200

    New version method on tog gem

--- lib/tog/version.rb
+++ lib/tog/version.rb
@@ -3,6 +3,16 @@ module Tog
     MAJOR = 0
     MINOR = 1
     TINY  = 6
+    # http://en.wikipedia.org/wiki/Moons_of_Jupiter
+    CODENAME = 'Metis'
     STRING = [MAJOR, MINOR, TINY].join('.')
+    
+    class << self
+      def to_s
+        "#{CODENAME} #{STRING}"
+      end
+      alias :to_str :to_s
+    end
+    
   end
 end
